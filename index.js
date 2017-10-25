4e5d22e0f1947cd9a3630f73109cbc18c537440e
Parv Kumar <parvathikumar92@gmail.com>
2017-10-25 18:09:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+function produceDrivingRange(distance){
+  return function(beg, end){
+    let first = parseInt(beg)
+    let last = parseInt(end);
+    let difference = Math.abs(first-last)
+    let range = Math.abs(difference - distance)
+    if(difference <= distance){
+      return `within range by ${range}`
+    }
+    else{
+      return `${range} blocks out of range`
+    }
+  }
+}
+
+function produceTipCalculator(percent){
+  return function(total){
+    return total * percent
+  }
+
+
+}
